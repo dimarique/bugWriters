e401c6042e967e50b40a4bd419db3980a85e6e5d
shapovalova <lenchik.sha.vse@gmail.com>
2025-08-07 16:43:13 +0200

    fixesComponentHero

--- src/components/HeroBlock/Hero.jsx
+++ src/components/HeroBlock/Hero.jsx
@@ -7,12 +7,10 @@ const Hero = () => {
 
   return (
 
-    <div className={styles.hero_wrapper}>
-      <div className={styles.hero_overlay}>
-        <h1 className={styles.hero_title}>Amazing Discounts on Garden Products!</h1>
+    <div className={styles.hero_container}>
+      <h1 className={styles.hero_title}>Amazing Discounts on Garden Products!</h1>
         <a className={styles.hero_link} href="#">Check out</a>
       </div>
-    </div>
   )
 
 
