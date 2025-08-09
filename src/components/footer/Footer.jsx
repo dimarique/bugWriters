85943b69ea419a02635b31c1e1468f742babdfe7
Jana Walt <you@example.com>
2025-08-09 11:37:35 +0200

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,8 @@ import styles from './footer.module.css'
 import instagram from '../../assets/icons/ic-instagram.svg'
 import whatsapp from '../../assets/icons/ic-whatsapp.svg'
 
+
+
 const Footer = () => {
     return (
         <div className={styles.footer_container}>
@@ -36,9 +38,9 @@ const Footer = () => {
                 </div>
 
 
-                <div>
-                    <iframe className={styles.footer_map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.407622818629!2d13.37250577564544!3d52.50796183712204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851cbdeaf3909%3A0xff2aef2e44148447!2sLinkstra%C3%9Fe%202%2C%2010785%20Berlin!5e0!3m2!1sru!2sde!4v1754509277036!5m2!1sru!2sde" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
-                </div>
+
+                <iframe className={`${styles.footer_map} section`} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.407622818629!2d13.37250577564544!3d52.50796183712204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851cbdeaf3909%3A0xff2aef2e44148447!2sLinkstra%C3%9Fe%202%2C%2010785%20Berlin!5e0!3m2!1sru!2sde!4v1754509277036!5m2!1sru!2sde" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+
 
             </div>
 
@@ -48,3 +50,5 @@ const Footer = () => {
 }
 
 export default Footer
+
+
