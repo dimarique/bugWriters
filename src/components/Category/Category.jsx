e1de7d430781d78d4ca9e29ac6d1d07dcd5b59e8
shapovalova <lenchik.sha.vse@gmail.com>
2025-08-10 18:34:45 +0200

    category fixed

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,51 +1,59 @@
-import React from 'react'
-import style from './Category.module.css'
-import Fertilizer from '../../assets/imageCategory/fertilizer.png'
-import ProtectiveProducts from '../../assets/imageCategory/protectiveProducts.png'
-import PlantingMaterial	from '../../assets/imageCategory/plantingMaterial.png'
-import ToolsAndEquipment from '../../assets/imageCategory/toolsAndEquipment.png'
-
-
-
+import React from "react";
+import style from "./Category.module.css";
+import Fertilizer from "../../assets/imageCategory/fertilizer.png";
+import ProtectiveProducts from "../../assets/imageCategory/protectiveProducts.png";
+import PlantingMaterial from "../../assets/imageCategory/plantingMaterial.png";
+import ToolsAndEquipment from "../../assets/imageCategory/toolsAndEquipment.png";
 
 const Category = () => {
   return (
     <div className={style.category}>
-
       <div className={style.category_header}>
         <h2 className={style.category_header_title}>Categories</h2>
-        <div className={style.category_header_buttons}>
-          <button className={style.category_header_button}>All categories</button>
-        </div>
 
+        <div className={style.category_header_buttons}>
+          <hr className={style.category_header_line} />
+          <button className={style.category_header_button}>
+            All categories
+          </button>
+        </div>
       </div>
 
       <div className={style.category_content}>
         <div className={style.category_content_item}>
-            <img src={Fertilizer} alt="Fertilizer" />
-            <a href="#"><p>Fertilizer</p></a>
+          <div className={style.category_content_item}>
+            <a href="#">
+              <img src={Fertilizer} alt="Fertilizer" />
+              <span>Fertilizer</span>
+            </a>
+          </div>
         </div>
 
         <div className={style.category_content_item}>
+          <a href="#">
             <img src={ProtectiveProducts} alt="Protective Products" />
-            <a href="#"><p>Protective products and septic tanks</p></a>
+            <span>Protective products and septic tanks</span>
+          </a>
         </div>
 
         <div className={style.category_content_item}>
+          <a href="#">
             <img src={PlantingMaterial} alt="Planting Material" />
-            <a href="#"><p>Planting Material</p></a>
+            <span>Planting Material</span>
+          </a>
         </div>
 
         <div className={style.category_content_item}>
+          <a href="#">
             <img src={ToolsAndEquipment} alt="Tools and Equipment" />
-            <a href="#"><p>Tools and Equipment</p></a>
+            <span>Tools and Equipment</span>
+          </a>
         </div>
 
         <button className={style.category_button}>All categories</button>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
