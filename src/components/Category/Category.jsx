57816fba7a6cbab9fe056b25c168cf59c09f2a00
shapovalova <lenchik.sha.vse@gmail.com>
2025-08-10 15:51:53 +0200

    category change

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,39 +1,47 @@
 import React from 'react'
 import style from './Category.module.css'
-import Fertilizer from './image/fertilizer.png'
-import ProtectiveProducts from './image/protectiveProducts.png'
-import PlantingMaterial	from './image/plantingMaterial.png'
-import ToolsAndEquipment from './image/toolsAndEquipment.png'
+import Fertilizer from '../../assets/imageCategory/fertilizer.png'
+import ProtectiveProducts from '../../assets/imageCategory/protectiveProducts.png'
+import PlantingMaterial	from '../../assets/imageCategory/plantingMaterial.png'
+import ToolsAndEquipment from '../../assets/imageCategory/toolsAndEquipment.png'
 
 
 
 
 const Category = () => {
   return (
-    <div className={style.category_overlay}>
+    <div className={style.category}>
 
-      <h2 className={style.category_title}>Categories</h2>
+      <div className={style.category_header}>
+        <h2 className={style.category_header_title}>Categories</h2>
+        <div className={style.category_header_buttons}>
+          <button className={style.category_header_button}>All categories</button>
+        </div>
+
+      </div>
 
       <div className={style.category_content}>
-        <div>
+        <div className={style.category_content_item}>
             <img src={Fertilizer} alt="Fertilizer" />
             <a href="#"><p>Fertilizer</p></a>
         </div>
 
-        <div>
+        <div className={style.category_content_item}>
             <img src={ProtectiveProducts} alt="Protective Products" />
             <a href="#"><p>Protective products and septic tanks</p></a>
         </div>
 
-        <div>
+        <div className={style.category_content_item}>
             <img src={PlantingMaterial} alt="Planting Material" />
             <a href="#"><p>Planting Material</p></a>
         </div>
 
-        <div>
+        <div className={style.category_content_item}>
             <img src={ToolsAndEquipment} alt="Tools and Equipment" />
             <a href="#"><p>Tools and Equipment</p></a>
         </div>
+
+        <button className={style.category_button}>All categories</button>
 
       </div>
     </div>
