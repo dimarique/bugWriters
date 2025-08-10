58ea533ffcbccdd54895bf1832d27b8a3cc90a51
dimarique <pochtagreen@gmail.com>
2025-08-10 19:14:57 +0200

    add Category to Main page, remove components from App.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,12 @@
 import styles from "./Main.module.css";
 import Hero from "../../components/HeroBlock/Hero.jsx";
 import Footer from "../../components/footer/footer.jsx";
+import Category from "../../components/Category/Category.jsx";
 const Main = () => {
   return (
     <div className={styles.main}>
       <Hero />
+      <Category />
       <Footer />
     </div>
   );
