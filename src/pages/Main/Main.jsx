333efc5840ec9a5c8f31e4896530e93c24c6d775
dimarique <pochtagreen@gmail.com>
2025-08-15 12:07:11 +0200

    remove unnecessary imports from App and Main

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,7 +2,6 @@ import styles from "./Main.module.css";
 import Hero from "../../components/HeroBlock/Hero.jsx";
 import Category from "../../components/Category/Category.jsx";
 import DiscountForm from "../../components/DiscountForm/DiscountForm.jsx";
-import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 import SaleSection from "../../components/SaleSection/SaleSection.jsx";
 const Main = () => {
   return (
