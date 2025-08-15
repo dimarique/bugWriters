eb44932c7723bfd1d9af490c2e60f53c797e42da
dimarique <pochtagreen@gmail.com>
2025-08-15 14:23:26 +0200

    add custom hook for random product

--- src/components/SaleSection/SaleSection.jsx
+++ src/components/SaleSection/SaleSection.jsx
@@ -1,44 +1,32 @@
 import styles from "./SaleSection.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { fetchProducts } from "../../redux/slices/productsSlice.js";
 import ProductCard from "../ProductCard/ProductCard.jsx";
+import { useRandomDiscountProducts } from "../../hooks/useRandomDiscountProducts.js";
 
 const SaleSection = () => {
-  const [randomItems, setRandomItems] = useState([]);
   const products = useSelector((state) => state.products.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchProducts());
-  }, []);
+  }, [dispatch]);
 
-  useEffect(() => {
-    if (products.length > 0) {
-      const selectedProducts = new Set();
+  const randomItems = useRandomDiscountProducts(products, 4);
 
-      while (selectedProducts.size < 4) {
-        let idx = Math.floor(Math.random() * products.length);
-        products[idx].discont_price && selectedProducts.add(products[idx]);
-      }
-
-      setRandomItems(Array.from(selectedProducts));
-    }
-  }, [products]);
   return (
     <div className={`${styles.saleSection} side_padding`}>
-      {randomItems.map((product) => {
-        return (
-          <ProductCard
-            key={self.crypto.randomUUID()}
-            id={product.id}
-            title={product.title}
-            price={product.price}
-            image={product.image}
-            discont_price={product.discont_price}
-          />
-        );
-      })}
+      {randomItems.map((product) => (
+        <ProductCard
+          key={self.crypto.randomUUID()}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          image={product.image}
+          discont_price={product.discont_price}
+        />
+      ))}
     </div>
   );
 };
