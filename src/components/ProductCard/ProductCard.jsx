8c311ec6346f30cc6c04d8b1652963c0366b397b
dimarique <pochtagreen@gmail.com>
2025-08-20 14:36:29 +0200

    crm59_add dynamic logic to ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,12 +4,14 @@ import cart_filled from "../../assets/cart_filled.svg";
 import DiscountAmountBage from "../DiscountAmountBage/DiscountAmountBage";
 
 const ProductCard = ({ title, price, discont_price, image }) => {
-  const baseUrl = "http://localhost:3333";
+  const baseUrl = import.meta.env.VITE_API_URL;
 
   return (
     <div className={styles.productCard}>
       <div className={styles.productCard_top}>
-        <DiscountAmountBage price={price} discont_price={discont_price} />
+        {discont_price && (
+          <DiscountAmountBage price={price} discont_price={discont_price} />
+        )}
         <span className={styles.productCard_favorite}>
           <img src={heart_filled} alt="" />
         </span>
@@ -22,12 +24,18 @@ const ProductCard = ({ title, price, discont_price, image }) => {
       <div className={styles.productCard_bottom}>
         <p className={styles.productCard_title}>{title}</p>
         <div className={styles.prices_wrapper}>
-          <span
-            className={styles.productCard_discont_price}
-          >{`$${discont_price}`}</span>
           {discont_price && (
-            <span className={styles.productCard_price}>{`$${price}`}</span>
+            <span
+              className={styles.productCard_discont_price}
+            >{`$${discont_price}`}</span>
           )}
+          <span
+            className={
+              discont_price
+                ? styles.productCard_price
+                : styles.productCard_discont_price
+            }
+          >{`$${price}`}</span>
         </div>
       </div>
     </div>
