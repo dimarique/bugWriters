4aa3f2c98fd1dcb3399131456b09fe629727d280
shapovalova <lenchik.sha.vse@gmail.com>
2025-08-22 14:35:05 +0200

    crm52_sortProduct_Component

--- src/redux/slices/productsSlice.js
+++ src/redux/slices/productsSlice.js
@@ -18,7 +18,23 @@ export const productsSlice = createSlice({
     error: "",
     showDiscount: false,
   },
-  reducers: {},
+  reducers: {
+    showDiscount(state, action) {
+      // is waiting for true/false
+      state.showDiscount = action.payload
+    },
+    compareProducts: (state, action) => {
+      const { sortBy } = action.payload; //Это динамическое значение: оно меняется каждый раз в зависимости от выбора.
+    //  console.log( sortBy);  !!!!! таки меняется
+      state.products.sort((a, b) => { //.sort мутирует массив
+        if (sortBy === "nameAsc") return a.title.localeCompare(b.title);// сравниваю строки по алфавиту
+        if (sortBy === "nameDesc") return b.title.localeCompare(a.title);
+        if (sortBy === "priceAsc") return a.price - b.price;
+        if (sortBy === "priceDesc") return b.price - a.price;
+        return 0;
+      });
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchProducts.pending, (state, action) => {
@@ -34,4 +50,6 @@ export const productsSlice = createSlice({
       });
   },
 });
+export const { compareProducts } = productsSlice.actions;
+export const { showDiscount } = productsSlice.actions;
 export default productsSlice.reducer;
