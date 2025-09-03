5bbbb72723595acbba9bf6cfc206248b5051391e
Jana Walt <you@example.com>
2025-09-03 18:56:45 +0200

    crm78_work_on_responsive_design

--- src/components/FavoritesIcon/FavoritesIcon.jsx
+++ src/components/FavoritesIcon/FavoritesIcon.jsx
@@ -1,12 +1,13 @@
 import styles from "./FavoritesIcon.module.css";
 import heart from "../../assets/heart.svg";
-
+import { useSelector } from "react-redux";
 const FavoritesIcon = () => {
+const count = useSelector((state)=> state.favorites.favProducts.length)
   return (
     <div className={styles.favoritesIcon}>
       <img src={heart} alt="favorites" />
+      {count > 0 && <span className={styles.countBage}>{count}</span>}
     </div>
   );
 };
-
 export default FavoritesIcon;
