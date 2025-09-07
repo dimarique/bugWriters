0f4c3d80b645f9fd14c3f51fdbde0e89526829cb
dimarique <pochtagreen@gmail.com>
2025-09-07 16:40:31 +0200

    add slice logic to FilterByPrice component

--- src/components/FilterByPrice/FilterByPrice.jsx
+++ src/components/FilterByPrice/FilterByPrice.jsx
@@ -1,26 +1,46 @@
 import styles from "./FilterByPrice.module.css";
-import { useDispatch } from "react-redux";
-import { setPriceFrom, setPriceTo } from "../../redux/slices/productsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setPriceFrom, setPriceTo } from "../../redux/slices/filtersSlice";
+import { useState, useEffect } from "react";
 
 const FilterByPrice = () => {
+  const [localPriceFrom, setLocalPriceFrom] = useState(0);
+  const [localPriceTo, setLocalPriceTo] = useState(Infinity);
+
+  const { priceFrom, priceTo } = useSelector((state) => state.filters);
   const dispatch = useDispatch();
 
-  const handleFromInputChange = (e) => dispatch(setPriceFrom(e.target.value));
-  const handleToInputChange = (e) => dispatch(setPriceTo(e.target.value));
+  const handleLocalPriceFromChange = (e) => setLocalPriceFrom(e.target.value);
+  const handleLocalPriceToChange = (e) => setLocalPriceTo(e.target.value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(setPriceFrom(localPriceFrom));
+      return clearTimeout(timer);
+    }, 1000);
+    console.log(priceFrom, priceTo);
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(setPriceTo(localPriceTo));
+      return clearTimeout(timer);
+    }, 500);
+  });
 
   return (
     <div className={styles.filterByPrice}>
       <span>Price</span>
       <input
         className={styles.input_price}
-        onChange={handleFromInputChange}
+        onChange={handleLocalPriceFromChange}
         id="price_from"
         type="number"
         placeholder="from"
       />
       <input
         className={styles.input_price}
-        onChange={handleToInputChange}
+        onChange={handleLocalPriceToChange}
         id="price_to"
         type="number"
         placeholder="to"
