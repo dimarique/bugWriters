53a16acbb2a2471d4ad5c4cbe80e380b14d71e40
dimarique <pochtagreen@gmail.com>
2025-09-07 15:54:46 +0200

    create base filtersSlice

---
+++ src/redux/slices/filtersSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const filtersSlice = createSlice({
+  name: "filters",
+  initialState: {
+    priceFrom: 0,
+    priceTo: Infinity,
+    isDiscounted: false,
+  },
+  reducers: {
+    setPriceFrom: (state, action) => {
+      state.priceFrom = action.payload;
+    },
+    setPriceTo: (state, action) => {
+      state.priceTo = action.payload;
+    },
+    setIsDiscounted: (state, action) => {
+      state.isDiscounted = action.payload;
+    },
+  },
+});
+
+export const { setPriceFrom, setPriceTo, setIsDiscounted } =
+  filtersSlice.actions;
+
+export default filtersSlice.reducer;
